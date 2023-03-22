7639fc6741aba258280a074eaae791a69ce41a43
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-03-22 11:42:51 +0530

    next geater element.js

--- AdvancedProblemSolving/16 mar-20/next greater element.js
+++ AdvancedProblemSolving/16 mar-20/next greater element.js
@@ -1,20 +1,20 @@
 function nextGreater(arr) {
     let n = arr.length;
     let st = [];
-    let ouput = [];
+    let output = [];
     st.push(0);
     for(let i = 1; i < n; i++) {
         while(st.length > 0 && arr[i] > arr[st[st.length - 1]]) {
-            ouput[st[st.length - 1]] = arr[i];
+            output[st[st.length - 1]] = arr[i];
             st.pop();
         }
         st.push(i);
     }
     while(st.length > 0) {
-        ouput[st[st.length - 1]] = - 1;
+        output[st[st.length - 1]] = - 1;
         st.pop();
     }
-    return ouput;
+    return output;
 }
 
 let arr = [3,5,10,1,2,9];
