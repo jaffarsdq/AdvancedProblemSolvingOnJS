fffb885b5be290a9b3340e1d342455109b224516
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-03-29 19:46:28 +0530

    rotate Array

--- AdvancedProblemSolving/2_Feb_1/rotateArray.js
+++ AdvancedProblemSolving/2_Feb_1/rotateArray.js
@@ -2,30 +2,32 @@
 // Output: [5,6,7,1,2,3,4]
 let nums = [1,2,3,4,5,6,7];
 let k = 3;
-console.log( rotate (nums,k))
 
-function rotate (nums,k){
-
-    let i=0 , j = nums.length-1-k;
-    while(i < j){
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length;
+    let i = 0, j = nums.length - 1;
+    while(i < j) {
         let temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
         i++;
         j--;
     }
-    i = nums.length-k;
-    j = nums.length-1;
-    while(i < j){
+    i = 0, j = k - 1;
+    while(i < j) {
         let temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
         i++;
         j--;
     }
-    i = 0;
-    j = nums.length-1;
-    while(i < j){
+    i = k, j = nums.length - 1;
+    while(i < j) {
         let temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
@@ -33,6 +35,7 @@ function rotate (nums,k){
         j--;
     }
     return nums;
+};
 
-    
-}
+
+console.log(rotate(nums, k));
